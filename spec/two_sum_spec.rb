53b9ef87eb33b4efe76a7118e127bb81533293df
Jason Reeder <info@jasonreeder.com>
2017-02-02 16:10:57 -0800

    Correct method call

--- spec/two_sum_spec.rb
+++ spec/two_sum_spec.rb
@@ -4,10 +4,10 @@ describe '#bad_two_sum?' do
   let(:arr) { [0, 1, 5, 7] }
 
   it 'returns true when any two numbers in array sum to number' do
-    expect(two_sum?(arr, 6)).to be(true)
+    expect(bad_two_sum?(arr, 6)).to be(true)
   end
 
   it 'returns false when no two numbers in array sum to number' do
-    expect(two_sum?(arr, 10)).to be(false)
+    expect(bad_two_sum?(arr, 10)).to be(false)
   end
 end
